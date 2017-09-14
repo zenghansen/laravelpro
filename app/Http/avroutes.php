596895c932c18d3f5b6da7e8ef2fee1e5a3f641f
zenghansen <zenghansen@aipai.com>
2017-09-14 14:45:24 +0800

    routes can set

---
+++ app/Http/avroutes.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+    return view('welcome');
+});
+//Route::get('user/{id}', 'Jg\UserController@showProfile');
+Route::group(['prefix' => 'av', 'middleware' => ['addheader']], function () {
+
+    Route::group(['middleware' => ['auth', 'role']], function () {
+
+
+        Route::group(['prefix' => 'customer'], function () {
+            Route::get('index', 'Jg\CustomerController@tpl');
+            Route::get('list', 'Jg\CustomerController@getList');
+            Route::post('edit', 'Jg\CustomerController@editRow');
+        });
+        Route::group(['prefix' => 'user'], function () {
+            Route::get('index', 'Jg\UserController@tpl');
+            Route::get('list', 'Jg\UserController@getList');
+            Route::post('edit', 'Jg\UserController@editRow');
+        });
+    });
+
+    Route::group(['prefix' => 'login'], function () {
+        Route::get('index', 'Jg\LoginController@tpl');
+        Route::post('login', 'Jg\LoginController@login');
+        Route::get('logout', 'Jg\LoginController@logout');
+    });
+});
